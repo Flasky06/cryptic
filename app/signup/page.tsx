98f28a0b31c66c71db0f9bc4f881b69e65eb6c5b
flasky06 <bonnienjuguna106@gmail.com>
2024-03-12 11:01:17 +0300

    hanled form inouts

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,29 +1,54 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Link from "next/link";
 import { FaArrowRight } from "react-icons/fa";
 
-function page() {
+function SignupPage() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  console.log(name);
+  console.log(email);
+  console.log(password);
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+
+    if (!name || !email || !password) {
+      setError("all fields are required");
+    }
+  };
   return (
     <div className="flex mx-auto justify-center max-w-3xl mt-10 ">
-      <div className="shadow-lg p-5 rounded-lg border-t-4 border-b-2 border-gray-400">
+      <div className="shadow-lg p-5 rounded-lg border-t-4 border- b-2 border-gray-400">
         <h1 className="text-sm text-center my-4">Hi, Please Sign in</h1>
 
-        <form className="flex flex-col gap-3">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
           <input
+            onChange={(e) => setName(e.target.value)}
             type="text"
             placeholder="username"
             className="border border-gray-400 rounded px-2 py-1"
           />
           <input
+            onChange={(e) => setEmail(e.target.value)}
             type="text"
             placeholder="Email"
             className="border border-gray-400 rounded px-2 py-1"
           />
           <input
+            onChange={(e) => setPassword(e.target.value)}
             type="password"
             placeholder="password"
             className="border border-gray-400 rounded px-2 py-1"
           />
+          {error && (
+            <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2 ">
+              {error}
+            </div>
+          )}
           <p className="p-1 text-sm">
             already have an account?
             <Link href="/login" className="text-base font-bold">
@@ -40,4 +65,4 @@ function page() {
   );
 }
 
-export default page;
+export default SignupPage;
